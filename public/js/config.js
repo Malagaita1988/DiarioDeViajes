64eb5f9c05374e90a04b61a2a19d22fbe3012dca
Malagaita988 <malagaita1988@gmail.com>
2025-02-07 18:11:23 +0100

    Actualizar config.js, forms.js y details.js para usar la URL de producción correctamente

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,6 @@
+
 const API_BASE_URL = window.location.hostname === "localhost"
   ? "http://localhost:4000"
   : "https://diariodeviajes.onrender.com";
+window.API_BASE_URL = API_BASE_URL;
+console.log("API_BASE_URL:", API_BASE_URL);
